26b97e88c04c95c7603cff67c392074a24aad530
jwb005 <54551453+jwb005@users.noreply.github.com>
2019-08-27 15:44:43 -0700

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,21 +1,16 @@
 <?php
+	if(isset($_POST['submit'])){
 	$name = $_POST['name'];
-	$visitor_email = $_POST['email'];
+	$subject= $_POST['subject'];
+	$mailFrom= $_POST['mail'];
 	$message = $_POST['message'];
 	
-	$email_from = 'b2stdv@gmail.com';
+	$mailTo = 'b2stdv@gmail.com';
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recevied an e-mail from ".$name."\n\n".$message;
+
 	
-	$email_subject = "New Form Submission";
-	
-	$email_body = "User Name: $name.\n".
-					"User Email: $visitor_email.\n".
-						"User Message: $message.\n";
-						
-	$to = "jwb005@ucsd.edu";
-	
-	$headers = "From: $email_from \r\n";
-	$headers .= "Reply-To: $visitor_email \r\n";
-	
-	mail($to,$email_subject,$email_body,$headers);
-	
-	header("Location: My Website.html");
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsnd");
+}
+
